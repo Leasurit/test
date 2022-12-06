0df764fbd5d7c52270bf36521d29e15e5a19ca15
Leasurit <116798710+Leasurit@users.noreply.github.com>
2022-12-06 12:40:27 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,9 @@
 import streamlit as st
+
+st.title("First try")
 st.write('Hello world')
+option = st. selectbox
+    ('My favourite fruit:', ('Strawberry', 'Raspberry', 'Blueberry'))
+option = st.selectbox(
+
+st.write('You chose:', option)
