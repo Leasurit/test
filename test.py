2a587a279c29369ebe9a9b08e45722ed4bd9d536
Leasurit <116798710+Leasurit@users.noreply.github.com>
2022-12-06 12:52:51 +0100

    Update test.py

--- test.py
+++ test.py
@@ -5,8 +5,7 @@ import numpy as np
 st.title("First try")
 st.write('Hello world')
 option = st. selectbox ('My favourite fruit:', ('Strawberry', 'Raspberry', 'Blueberry'))
-
-st.write('You chose:', option)
+st.write('Selected:', option)
 
 arr = np.random.normal(1, 1, size=100)
 fig, ax = plt.subplots()
