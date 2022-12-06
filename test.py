abaa59ca84d6ed5db351aea133fc2cc270c2d8a0
Leasurit <116798710+Leasurit@users.noreply.github.com>
2022-12-06 12:44:01 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,15 @@
 import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
 
 st.title("First try")
 st.write('Hello world')
-option = st. selectbox
-    ('My favourite fruit:', ('Strawberry', 'Raspberry', 'Blueberry'))
-option = st.selectbox(
+option = st. selectbox ('My favourite fruit:', ('Strawberry', 'Raspberry', 'Blueberry'))
 
 st.write('You chose:', option)
+
+arr = np.random.normal(1, 1, size=100)
+fig, ax = plt.subplots()
+ax.hist(arr, bins=20)
+
+st.pyplot(fig)
