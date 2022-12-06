1fa00893051f925370b438552a051caf081ffe23
Leasurit <116798710+Leasurit@users.noreply.github.com>
2022-12-06 13:08:27 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+
 
 st.title("First try")
 st.write('Hello world')
@@ -8,7 +10,13 @@ option = st. selectbox ('My favourite fruit:', ('Strawberry', 'Raspberry', 'Blue
 st.write('Selected:', option)
 
 x = np.linspace(0, 50, 50)
-fig, ax = plt.subplots()
+fig1, ax = plt.subplots()
 ax.plot(x, x**2)
 
 st.pyplot(fig)
+
+df = pd.read_csv('test/BastarCraton.csv')
+x2 = df.loc('Mg')
+y = df.loc('Si')
+fig2, ax = plt.subplots()
+ax.plot(x2, y)
