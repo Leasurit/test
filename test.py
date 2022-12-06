7260bc9c0eefeebbe947ddc65955b2aa7a81d140
Leasurit <116798710+Leasurit@users.noreply.github.com>
2022-12-06 13:09:58 +0100

    Update test.py

--- test.py
+++ test.py
@@ -13,10 +13,12 @@ x = np.linspace(0, 50, 50)
 fig1, ax = plt.subplots()
 ax.plot(x, x**2)
 
-st.pyplot(fig)
+st.pyplot(fig1)
 
 df = pd.read_csv('BastarCraton.csv')
 x2 = df.loc('Mg')
 y = df.loc('Si')
 fig2, ax = plt.subplots()
 ax.plot(x2, y)
+
+st.pyplot(fig2)
